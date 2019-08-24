4a3aa168d950c2820e06f14023974af0e459123a
Safdar Jamal <safdar.dev@gmail.com>
2019-08-24 21:22:27 +0500

    Convert App class to function

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { store } from '../../store';
 
 import HelloWorld from '../HelloWorld';
 import ButtonGroup from '../ButtonGroup';
 
-class App extends Component {
-  render() {
-    // console.log(store.getState());
+const App = () => {
+  // console.log(store.getState());
 
-    return [
-      <HelloWorld key={1} tech={store.getState().tech} />,
-      <ButtonGroup key={2} technologies={['React', 'Redux', 'React-Redux']} />
-    ];
-  }
-}
+  return [
+    <HelloWorld key={1} tech={store.getState().tech} />,
+    <ButtonGroup key={2} technologies={['React', 'Redux', 'React-Redux']} />
+  ];
+};
 
 export default App;
